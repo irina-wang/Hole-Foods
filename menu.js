b447e7d284a906feefc767258c0f87146a59daab
Cindy Siu <cindy@Cindys-MacBook-Pro-2.local>
2020-02-29 15:28:06 -0500

    added hamburger menu

---
+++ menu.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.menu-ham').click(function () {
+        $('.menu').animate({left: '0px'}, 100)
+    });
+    $('.close-menu').click(function () {
+        $('.menu').animate({left: '-650px'}, 100)
+});
+});
